38238007644eff304814ccbe9008ef4f4f4b1bf5
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 12:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+const sourceArray= [1, 2, 3, -9];
+const array= [];
+
+function mapToNegativize(array) {
+    let result = array.map(function(number){
+        return -number ;
+})
+    return result;
+};
+
+function mapToDouble(array) {
+    let result = array.map(function(number){
+        return number * 2 ;
+})
+    return result;
+};
