3ead3ce72e0685322e5ec1935c48ba7d8ffe93ad
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 13:19:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map (array, func) {
+    let r = [];
+    for (let i = 0; i < array.length; i++ ) {
+      r.push(func(array[i]));
+    }
+    return r;
+}
+
+function reduce (array, func, startingPoint = 0) {
+	let newValue = startingPoint;
+    for (let i = 0; i < array.length; i++ ) {
+        if(array[i] === false){
+            newValue = false;
+            continue
+        } else if (array[i] === true){
+            newValue = true;
+            continue
+        }
+      newValue = func(array[i],newValue);
+    }
+    return newValue;
+}
